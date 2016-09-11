758799679f89a9831d58e81bd0009e373c1aeb3d
Berk Duran <duranmberk@gmail.com>
2016-09-11 13:20:42 -0400

    fixed the test case

--- test/strategydemo/TestFlatRateDiscountStrategy.java
+++ test/strategydemo/TestFlatRateDiscountStrategy.java
@@ -9,7 +9,7 @@ public class TestFlatRateDiscountStrategy {
 	@Test
 	public void testFlatRateDiscount1() {
 		Order order = new Order("John", 200.24f);
-		float discount = 5; 
+		float discount = 10; 
 		FlatRateDiscountStrategy strategy = new FlatRateDiscountStrategy(discount);
 		order.setStrategy(strategy);
 		assertEquals(190.24, order.getDiscountedPrice(), 0.00001);
